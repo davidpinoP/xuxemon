52709adff74885b33b096061dff74c1a68ae2e9e
Rocky0524 <kenbalboa05@gmail.com>
2026-02-27 19:31:45 +0100

    componente

---
+++ Frontend/xuxemons/src/app/components/info-usuario/info-usuario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-usuario',
+  imports: [],
+  templateUrl: './info-usuario.html',
+  styleUrl: './info-usuario.css',
+})
+export class InfoUsuario {
+
+}
