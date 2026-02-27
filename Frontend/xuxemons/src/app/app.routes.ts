552e4b139c2f1a83318944be48913bff8bef82a8
anasilerna <anasmejahad15@alumnes.ilerna.com>
2026-02-27 20:03:43 +0100

    Añadir ruta registro

--- Frontend/xuxemons/src/app/app.routes.ts
+++ Frontend/xuxemons/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { Registro } from './registro/registro';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
+    { path: 'register', component: Registro },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
