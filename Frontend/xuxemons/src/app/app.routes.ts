6fb549ad1150cb587002fa44e5568d8c3708cbb2
anasilerna <anasmejahad15@alumnes.ilerna.com>
2026-02-27 20:15:08 +0100

    arreglar error ruta

--- Frontend/xuxemons/src/app/app.routes.ts
+++ Frontend/xuxemons/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
-import { InfoUsuarioComponent } from './components/info-usuario/info-usuario';
+import { Registro } from './registro/registro';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
-    { path: 'perfil', component: InfoUsuarioComponent },
+    { path: 'register', component: Registro },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
