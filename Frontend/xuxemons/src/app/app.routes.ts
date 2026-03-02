8b24968c147e729603e544b59771e544b2a61299
anasilerna <anasmejahad15@alumnes.ilerna.com>
2026-03-02 19:27:13 +0100

    componente home y ruta

--- Frontend/xuxemons/src/app/app.routes.ts
+++ Frontend/xuxemons/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Registro } from './registro/registro';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
     { path: 'register', component: Registro },
+    { path: 'home', component: Home },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
