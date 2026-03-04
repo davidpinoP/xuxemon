d16726e8baad0e6afa32d1d26726c6cb6597ca02
davidpinoP <pino7david@gmail.com>
2026-03-04 18:48:32 +0100

    generar Componentes mochila y xuxedex

---
+++ Frontend/xuxemons/src/app/xuxedex/xuxedex.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xuxedex',
+  imports: [],
+  templateUrl: './xuxedex.html',
+  styleUrl: './xuxedex.css',
+})
+export class Xuxedex {
+
+}
