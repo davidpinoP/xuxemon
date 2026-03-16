4331747b8247d43284115914717a4409b71ebaaf
davidpinoP <pino7david@gmail.com>
2026-03-16 19:25:44 +0100

    CRUD de inventario + ruta por admin

--- Backend/Xuxemon_Bknd/routes/api.php
+++ Backend/Xuxemon_Bknd/routes/api.php
@@ -31,6 +31,8 @@ Route::middleware(['ApiAuth', 'is_active'])->group(function () {
         // Gestión de Usuarios e Inventarios
         Route::get('/users', [UserController::class, 'index']);
         Route::post('/users/{id}/inventory', [UserController::class, 'updateInventory']);
+        Route::put('/users/{id}/inventory/{itemName}', [UserController::class, 'modifyItemInInventory']);
+        Route::delete('/users/{id}/inventory/{itemName}', [UserController::class, 'deleteItemFromInventory']);
 
         Route::get('/admin/dashboard', function () {
             return response()->json(['message' => 'Bienvenido, Administrador']);
