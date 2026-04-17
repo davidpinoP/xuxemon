da6822f72287add63b2fbb43b9739160314acce6
davidpinoP <pino7david@gmail.com>
2026-04-17 19:24:33 +0200

    Meta Tags

--- Frontend/xuxemons/src/app/app.routes.ts
+++ Frontend/xuxemons/src/app/app.routes.ts
@@ -8,21 +8,22 @@ import { authGuard } from './guards/auth.guard';
 import { Mochila } from './mochila/mochila';
 import { FriendsBasic } from './friends-basic/friends-basic';
 
-import { AdminPanelComponent } from './admin-panel/admin-panel'; 
+import { AdminPanelComponent } from './admin-panel/admin-panel';
 import { adminGuard } from './guards/admin-guard';
 
 export const routes: Routes = [
-    { path: 'login', component: Login },
-    { path: 'register', component: Registro },
-    { path: 'home', component: Home, canActivate: [authGuard] },
-    { path: 'xuxedex', component: Xuxedex, canActivate: [authGuard] },
-    { path: 'inventory', component: Mochila, canActivate: [authGuard] },
-    { path: 'friends', component: FriendsBasic, canActivate: [authGuard] },
-    { path: 'perfil', component: Perfil, canActivate: [authGuard] },
-    { 
-        path: 'admin', 
-        component: AdminPanelComponent, 
-        canActivate: [adminGuard] 
+    { path: 'login', component: Login, title: 'Entrar - Xuxemons' },
+    { path: 'register', component: Registro, title: 'Registrarse - Xuxemons' },
+    { path: 'home', component: Home, canActivate: [authGuard], title: 'Inicio - Xuxemons' },
+    { path: 'xuxedex', component: Xuxedex, canActivate: [authGuard], title: 'Xuxedex - Catalogo' },
+    { path: 'inventory', component: Mochila, canActivate: [authGuard], title: 'Mochila - Inventario' },
+    { path: 'friends', component: FriendsBasic, canActivate: [authGuard], title: 'Amigos' },
+    { path: 'perfil', component: Perfil, canActivate: [authGuard], title: 'Perfil' },
+    {
+        path: 'admin',
+        component: AdminPanelComponent,
+        canActivate: [adminGuard],
+        title: 'Administración'
     },
 
     { path: '', redirectTo: 'login', pathMatch: 'full' },
